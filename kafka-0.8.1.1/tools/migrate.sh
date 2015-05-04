1ff6e96c12c79dede899892ffd49293af36f2a93
hhraymond <skyhuang007@163.com>
2015-05-04 17:54:26 +0800

    delete deploy tools

--- kafka-0.8.1.1/tools/migrate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup ./bin/kafka-run-class.sh kafka.tools.KafkaMigrationTool --kafka.07.jar migrate/kafka_2.9.2-0.7.2.jar --zkclient.01.jar migrate/zkclient-0.1.jar --num.producers 6 --consumer.config=migrate/07consumer.properties --producer.config=migrate/08producer.properties --whitelist=r.3 &
